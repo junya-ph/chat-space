fc06b26954d7f958fba37953fbfb1afcbb20a122
junya-ph <77junya77@gmail.com>
2020-07-21 10:19:38 +0900

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 20200716045231) do
+=======
+ActiveRecord::Schema.define(version: 20200721010637) do
+>>>>>>> Stashed changes
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -28,6 +32,20 @@ ActiveRecord::Schema.define(version: 20200716045231) do
     t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
+<<<<<<< Updated upstream
+=======
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.string   "image"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
+
+>>>>>>> Stashed changes
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
@@ -44,4 +62,9 @@ ActiveRecord::Schema.define(version: 20200716045231) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+<<<<<<< Updated upstream
+=======
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
+>>>>>>> Stashed changes
 end
